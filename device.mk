0ff5dcc83ad44aabbab08964d9d97a3f8528ee7e
nanya <xccx_2023@hotmail.com>
2025-12-05 15:20:20 +0800

    add fox.mk

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# some OrangeFox-specific settings
+$(call inherit-product, $(LOCAL_PATH)/fox_sm86xx.mk)
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
