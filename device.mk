078d62b3f4a49e34655bf568cc1a904183e4544b
Nanya <xccx_2023@hotmail.com>
2025-12-27 12:36:54 +0800

    移除gsi key

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # some OrangeFox-specific settings
 $(call inherit-product, $(LOCAL_PATH)/fox_sm86xx.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL    := 34
 PRODUCT_SHIPPING_API_LEVEL  := 34
